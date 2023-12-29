8677dc2f18163bf42b39aca6bb8ccdacae96e4b9
chinmay patil <chinmay@mirro.ai>
2023-12-30 00:08:39 +0530

    added tooltips

--- src/views/newEntry/FileUpload.js
+++ src/views/newEntry/FileUpload.js
@@ -1,6 +1,6 @@
 import React, { useCallback, useState } from 'react';
 import { useDropzone } from 'react-dropzone';
-import { Box, Button, Paper, Typography } from '@mui/material';
+import { Box, Button, Paper, Tooltip, Typography } from '@mui/material';
 
 const FileUpload = ({ onFileUpload }) => {
   const [filePreviews, setFilePreviews] = useState([]);
@@ -33,43 +33,46 @@ const FileUpload = ({ onFileUpload }) => {
   };
 
   return (
-    <Box
-      sx={{
-        padding: '20px',
-        textAlign: 'center',
-        border: '1px dotted gray',
-        borderRadius: 5,
-        height: '100%',
-        pointerEvents: 'none',
-        opacity: 0.5
-      }}
-    >
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          gap: 15,
-          justifyContent: 'center',
-          alignItems: 'center'
+    <Tooltip title="Feature comming soon">
+      <Box
+        sx={{
+          padding: '20px',
+          textAlign: 'center',
+          border: '1px dotted gray',
+          borderRadius: 5,
+          height: '100%',
+
+          opacity: 0.5
         }}
-        {...getRootProps()}
       >
-        <input {...getInputProps()} />
-        <Typography variant="h6">Drag and drop a file here, or click to select a file</Typography>
-        <Button variant="outlined" color="primary" sx={{ borderRadius: 5 }}>
-          Upload File
-        </Button>
-      </div>
-      {filePreviews.map((preview, index) => (
-        <div key={index} style={{ marginTop: '10px' }}>
-          <img src={preview.preview} alt={`Preview ${index}`} style={{ width: '50px', height: '50px', marginRight: '10px' }} />
-          <span>{preview.file.name}</span>
-          <Button variant="outlined" color="secondary" onClick={() => removeFilePreview(index)}>
-            Remove
+        <div
+          style={{
+            pointerEvents: 'none',
+            display: 'flex',
+            flexDirection: 'column',
+            gap: 15,
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}
+          {...getRootProps()}
+        >
+          <input {...getInputProps()} />
+          <Typography variant="h6">Drag and drop a file here, or click to select a file</Typography>
+          <Button variant="outlined" color="primary" sx={{ borderRadius: 5 }}>
+            Upload File
           </Button>
         </div>
-      ))}
-    </Box>
+        {filePreviews.map((preview, index) => (
+          <div key={index} style={{ marginTop: '10px' }}>
+            <img src={preview.preview} alt={`Preview ${index}`} style={{ width: '50px', height: '50px', marginRight: '10px' }} />
+            <span>{preview.file.name}</span>
+            <Button variant="outlined" color="secondary" onClick={() => removeFilePreview(index)}>
+              Remove
+            </Button>
+          </div>
+        ))}
+      </Box>
+    </Tooltip>
   );
 };
 
