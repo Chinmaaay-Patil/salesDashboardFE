79dcd6050b19fb2e2a8c65adefdc36e2e2e7b2bc
chinmay patil <chinmay@mirro.ai>
2023-12-29 22:31:20 +0530

    added data rendering for customer list

--- src/utils/apiCalls/getSalesTrack.js
+++ src/utils/apiCalls/getSalesTrack.js
@@ -1,24 +1,53 @@
 import commonAPI from 'utils/axiosConfig';
 import { getSourceLeads } from './getSourceLeadsList';
+import { getSalesPersonList } from './getSalesPersonList';
+import { getStateList } from './getStateList';
+import { getVersionList } from './getVersionList';
 
 export const getSalesTrack = async () => {
   try {
     const response = await commonAPI.get(`/api/Dashboard/SalesList`);
 
-    const SalesList = response.data;
+    let SalesList = response.data;
 
-    const sourceLeadData = await getSourceLeads();
+    try {
+      const [sourceLeadData, salesPersonData, stateData, versionData] = await Promise.all([
+        getSourceLeads(),
+        getSalesPersonList(),
+        getStateList(),
+        getVersionList()
+      ]);
 
-    SalesList.forEach((element, index) => {
-      const sourceIdValue = element.sourceId;
-      const matchingElement = sourceLeadData.find((item) => item.sid === sourceIdValue);
-      if (matchingElement) {
-        SalesList[index].sourceId = { ...matchingElement };
-      }
-    });
+      SalesList = SalesList.map((element) => {
+        const sourceIdValue = element.sourceId;
+        const matchingElementSource = sourceLeadData.find((item) => item.sid === sourceIdValue);
 
-    console.log('chin', SalesList);
-    return SalesList;
+        const sourcePersonIdValue = element.salesPersonId;
+        const matchingElementSalesPerson = salesPersonData.find((item) => item.sid === sourcePersonIdValue);
+
+        const stateIdValue = element.stateId;
+        const matchingElementState = stateData.find((item) => item.stid === stateIdValue);
+
+        const versionIdValue = element.versionId;
+        const matchingElementVersion = versionData.find((item) => item.vid === versionIdValue);
+
+        return {
+          ...element,
+          sourceId: matchingElementSource ? { ...matchingElementSource } : null,
+          salesPersonId: matchingElementSalesPerson ? { ...matchingElementSalesPerson } : null,
+          stateId: matchingElementState ? { ...matchingElementState } : null,
+          versionId: matchingElementVersion ? { ...matchingElementVersion } : null
+        };
+      });
+      console.log('SalesList', SalesList);
+      return SalesList;
+    } catch (error) {
+      console.error('Error fetching getSalesTrack:', error.message);
+      throw error;
+    }
+
+    // console.log('chin', SalesList);
+    // return SalesList;
   } catch (error) {
     console.error('Error fetching getSalesTrack:', error.message);
     throw error;
